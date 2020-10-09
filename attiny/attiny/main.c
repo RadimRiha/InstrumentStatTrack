9e0bed146e027f648df608e2877b7f729adbf203
RadimRiha <radim.rihacek@seznam.cz>
2020-10-09 23:44:38 +0200

    LCD driver bugfix

--- attiny/attiny/main.c
+++ attiny/attiny/main.c
@@ -1,44 +1,43 @@
 #include <avr/io.h>
 #include <avr/sleep.h>
-#include <util/delay.h>
+#include <avr/interrupt.h>
 
 #define F_CPU 1000000UL
-#define sei() SREG |= (1 << 7)
-#define cli() SREG &= ~(1 << 7)
+#include <util/delay.h>
 
 //pins: DO-PA5 SCK-PA4 LE-PA7 IN-PA3,PB2 B1-PA0 B2-PA1
-#define LCD_BP 1
-#define LCD_1A 26
-#define LCD_1B 25
-#define LCD_1C 4
-#define LCD_1D 3
-#define LCD_1E 2
-#define LCD_1F 27
-#define LCD_1G 28
-#define LCD_1DP 5
-#define LCD_2A 31
-#define LCD_2B 24
-#define LCD_2C 0
-#define LCD_2D 6
-#define LCD_2E 7
-#define LCD_2F 30
-#define LCD_2G 29
-#define LCD_2DP 9
-#define LCD_3A 18
-#define LCD_3B 17
-#define LCD_3C 12
-#define LCD_3D 11
-#define LCD_3E 10
-#define LCD_3F 19
-#define LCD_3G 20
-#define LCD_3DP 15
-#define LCD_4A 23
-#define LCD_4B 16
-#define LCD_4C 8
-#define LCD_4D 13
-#define LCD_4E 14
-#define LCD_4F 22
-#define LCD_4G 21
+#define LCD_BP 25
+#define LCD_1A 2
+#define LCD_1B 1
+#define LCD_1C 28
+#define LCD_1D 27
+#define LCD_1E 26
+#define LCD_1F 3
+#define LCD_1G 4
+#define LCD_1DP 30
+#define LCD_2A 7
+#define LCD_2B 0
+#define LCD_2C 24
+#define LCD_2D 31
+#define LCD_2E 32
+#define LCD_2F 6
+#define LCD_2G 5
+#define LCD_2DP 17
+#define LCD_3A 10
+#define LCD_3B 9
+#define LCD_3C 20
+#define LCD_3D 19
+#define LCD_3E 18
+#define LCD_3F 11
+#define LCD_3G 12
+#define LCD_3DP 23
+#define LCD_4A 15
+#define LCD_4B 8
+#define LCD_4C 16
+#define LCD_4D 21
+#define LCD_4E 22
+#define LCD_4F 14
+#define LCD_4G 13
 
 const uint8_t LCDpinMap[32] = {
 LCD_4A, LCD_4B, LCD_4C, LCD_4D, LCD_4E, LCD_4F, LCD_4G, 0,
@@ -69,8 +68,8 @@ volatile uint8_t SPI_PHASE_FLAG = 0;
 uint8_t RELEASED_FLAG = 0;
 uint8_t SLEEPTIMERSTARTED_FLAG = 0;
 
-uint32_t LCDstates = 0;		//stores segment states, LCD_BP ignored
-volatile uint32_t SPIbuffer = 0;	//buffer for interrupt only
+uint32_t LCDstates = 0;				//stores segment states, LCD_BP ignored
+volatile uint32_t SPIbuffer = 0;	//buffer for LCD driver interrupt only
 
 #define DISP_OFF 255
 #define DISP_HOURS 254
@@ -89,32 +88,34 @@ uint8_t minuteCounter = 0;
 volatile uint8_t timer0OVFcounter = 0;
 
 
-void TIM1_COMPA_vect() {	//64Hz LCD driver
+ISR(TIM1_COMPA_vect) {	//64Hz LCD driver
 	//toggle BP and calculate buffer
 	if(SPI_PHASE_FLAG == 0) {	//phase 0
 		SPIbuffer = LCDstates;
-		SPIbuffer &= ~(SPI_PHASE_FLAG << LCD_BP);
+		SPIbuffer &= ~(1UL << LCD_BP);
 		SPI_PHASE_FLAG = 1;
 	}
 	else {	//phase 1
 		SPIbuffer = ~LCDstates;
-		SPIbuffer |= (SPI_PHASE_FLAG << LCD_BP);
+		SPIbuffer |= (1UL << LCD_BP);
 		SPI_PHASE_FLAG = 0;
 	}
 	//SPI transfer MSB first
-	uint8_t USIclock = USICR |= (1 << USICLK);
-	uint8_t USIshift = USICR |= (1 << USITC);
-	//uint8_t USIshift = USICR |= (1 << USITC) |= (1 << USICLK);
+	uint8_t USIclock = USICR | (1 << USITC);
+	uint8_t USIshift = USICR | (1 << USITC) | (1 << USICLK);
 	for (uint8_t byte = 0; byte <= 3; byte++) {
-		USIDR = (uint8_t)(SPIbuffer >> (byte * 8));
+		USIDR = (uint8_t)SPIbuffer;
 		for (uint8_t bit = 0; bit <= 7; bit++) {
 			USICR = USIclock;
 			USICR = USIshift;
 		}
+		SPIbuffer = SPIbuffer >> 8;
 	}
+	PORTA |= (1 << PA7);	//strobe LE
+	PORTA &= ~(1 << PA7);
 }
 
-void PCINT0_vect() {	//PCINT for button 1 and 2 (active low)
+ISR(PCINT0_vect) {	//PCINT for button 1 and 2 (active low)
 	if((PINA & (1 << PA0)) == 0) B1_FLAG = 1;
 	else {
 		B1_FLAG = 0;
@@ -127,16 +128,17 @@ void PCINT0_vect() {	//PCINT for button 1 and 2 (active low)
 	}
 }
 
-void TIM0_OVF_vect() {
+ISR(TIM0_OVF_vect) {
 	timer0OVFcounter++;
 }
 
 void initTimer1() {
 	TCCR1A = 0;
-	TCCR1B = (1 << WGM12);	//CTC operation
-	//TCCR1B |= (1 << CS11);		//1 prescaler
+	TCCR1B = (1 << WGM12);			//CTC operation
+	TCCR1B |= (1 << CS10);			//1 prescaler
 	OCR1A = (uint16_t)(F_CPU / 64);	//set up 64Hz interrupt frequency (maxval 65535)
-	TIMSK1 |= (1 << OCIE1A);			//enable COMPA interrupt
+	TIMSK1 |= (1 << OCIE1A);		//enable COMPA interrupt
+	TCNT1 = 0;						//reset timer
 }
 
 void initTimer0 () {
@@ -292,11 +294,12 @@ int main (void) {
 	initTimer0();
 	initUSI();
 	initButtons();
-	sei();
 	DDRA &= ~(1 << DDA3);	//IN input
 	hourCounter = loadEEPROM(ADDR_HOURCOUNTER_LO) | (loadEEPROM(ADDR_HOURCOUNTER_HI) << 8);
+	sei();
 	
     while (1) {
+		/*
 		if (!B1_FLAG & !B2_FLAG){	//inactivity
 			if (!SLEEPTIMERSTARTED_FLAG) {
 				SLEEPTIMERSTARTED_FLAG = 1;
@@ -343,6 +346,7 @@ int main (void) {
 				else if (release(2) && hourCounter > 0) hourCounter--;
 			break;
 		}
+	*/
     }
 }
 
